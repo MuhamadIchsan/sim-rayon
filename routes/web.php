85cb1ec8aae8c065325ac0f00560e2bdf0ba435a
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-10 22:03:01 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('auth.login');
 });
-Route::resource('dashboard','DashboardController');
-Route::resource('piket','PiketController');
-Route::resource('kumpul_rayon','KumpulRayonController'); 
-Route::resource('absen_piket','AbsenPiketController');
-Route::resource('absen_rayon','AbsenRayonController');
-Route::resource('rayon','RayonController');
-Route::resource('user','UserController');
+Route::middleware('auth')->group(function(){
+    Route::resource('dashboard','DashboardController');
+    Route::resource('piket','PiketController');
+    Route::resource('kumpul_rayon','KumpulRayonController'); 
+    Route::resource('absen_piket','AbsenPiketController');
+    Route::resource('absen_rayon','AbsenRayonController');
+    Route::resource('rayon','RayonController');
+    Route::resource('user','UserController');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
